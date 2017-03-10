b7284d261fd3fcb4f0cf43185b30fdf2651023d0
Trang Truong c <tnkttruong@gmail.com>
2017-03-10 19:15:31 +0700

    change exrcise_04

--- day_04_rb/exercise_04.rb
+++ day_04_rb/exercise_04.rb
@@ -1,15 +1,22 @@
 class Century  
 
-  def input_number
-    puts "Input the year"
-    @year = gets.chomp.to_i
-  end
+	def input_number
+		puts "Input the year"
+		@year = gets.chomp.to_i
 
-  def return_century
-  	input_number
-    puts "centuryFromYear(#{@year}) = #{@year/100}."
-  end
+	end
+
+	def return_century
+		input_number
+		if  @year%100 > 0
+			centuryFromYear = @year/100 + 1
+		else
+			centuryFromYear = @year/100
+		end
+		puts "centuryFromYear(#{@year}) = #{centuryFromYear}."
+	end
 end
 
 century = Century.new()
 century.return_century()
+
