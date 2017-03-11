3fec1c99ba0b3ec18a4c1e9acbfaa228fb7d589d
Trang Truong C <tnkttruong@gmail.com>
2017-03-11 20:41:50 +0700

    Update exercise_04.rb

--- day_04_rb/exercise_04.rb
+++ day_04_rb/exercise_04.rb
@@ -3,15 +3,17 @@ class Century
 	def input_number
 		puts "Input the year"
 		year = gets.chomp
-    while !checkInteger(year) do
-      puts "Please input a Year is a integer"
-      year = gets.chomp
-    end
-    return year.to_i
+		while !checkInteger(year) do
+			puts "Please input a Year"
+			year = gets.chomp
+		end
+		return year.to_i
 	end
 
 	def return_century(year)
-		if  year%100 > 0
+		if year == 0 
+			return 1
+		elsif  year%100 > 0
 			return year/100 + 1
 		else
 			return year/100
@@ -19,15 +21,14 @@ class Century
 	end
 
 	def checkInteger(number)
-    if number.match /^[0-9]+$/
-      return true
-    else false
-    end
-  end
+		if number.match /^[0-9]+$/
+			return true
+		else false
+		end
+	end
 end
 
 century = Century.new()
 year = century.input_number()
 centuryFromYear=century.return_century(year)
 puts "centuryFromYear(#{year}) = #{centuryFromYear}."
-
