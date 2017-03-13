ef0acef91d7da6bcc87eed8e607aa4adb6c66af5
Trang Truong c <tnkttruong@gmail.com>
2017-03-13 10:19:24 +0700

    fix space 2

--- day_04_rb/exercise_04.rb
+++ day_04_rb/exercise_04.rb
@@ -1,31 +1,31 @@
 class Century  
 
-	def input_number
-		puts "Input the year"
-		year = gets.chomp
-		while !checkInteger(year) do
-			puts "Please input a Year"
-			year = gets.chomp
-		end
-		return year.to_i
-	end
+  def input_number
+    puts "Input the year"
+    year = gets.chomp
+    while !checkInteger(year) do
+      puts "Please input a Year"
+      year = gets.chomp
+    end
+    return year.to_i
+  end
 
-	def return_century(year)
-		if year == 0 
-			return 1
-		elsif  year%100 > 0
-			return year/100 + 1
-		else
-			return year/100
-		end
-	end
+  def return_century(year)
+    if year == 0 
+      return 1
+    elsif  year%100 > 0
+      return year/100 + 1
+    else
+      return year/100
+    end
+  end
 
-	def checkInteger(number)
-		if number.match /^[0-9]+$/
-			return true
-		else false
-		end
-	end
+  def checkInteger(number)
+    if number.match /^[0-9]+$/
+      return true
+    else false
+    end
+  end
 end
 
 century = Century.new()
